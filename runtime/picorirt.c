95f83baf576697622d1a8ec1fdb6f63b725b695b
nenad <neno.markus@gmail.com>
2014-03-28 16:31:32 +0100

    update

--- runtime/picorirt.c
+++ runtime/picorirt.c
@@ -25,8 +25,13 @@
 
 #include <stdint.h>
 
+#ifndef MAX
 #define MAX(a, b) ((a)>(b)?(a):(b))
+#endif
+
+#ifndef MIN
 #define MIN(a, b) ((a)<(b)?(a):(b))
+#endif
 
 /*
 	
--- runtime/picort.c
+++ runtime/picort.c
@@ -27,8 +27,13 @@
 	
 */
 
+#ifndef MAX
 #define MAX(a, b) ((a)>(b)?(a):(b))
+#endif
+
+#ifndef MIN
 #define MIN(a, b) ((a)<(b)?(a):(b))
+#endif
 
 #define BINTEST(f, r, c, sr, sc, pixels, nrows, ncols, ldim) \
 		(	\
